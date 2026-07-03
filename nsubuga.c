cfb17cee5bf9e14f31807c245cef05700a07cd5a
Kafeero Mirembe Mercy <133584067+K-Mirembe-Mercy@users.noreply.github.com>
2026-07-03 12:07:00 +0300

    Add a C program to greet Nsubuga Ramathan

---
+++ nsubuga.c
@@ -0,0 +1,16 @@
+
+int main() {
+    printf("   HAPPY CODING, NSUBUGA RAMATHAN!  \n");
+
+    printf("Hey Nsubuga Ramathan! \n");
+    printf("You just ran your very own C program.\n");
+    printf("Keep pushing, great things are coming your way!\n\n");
+
+    for (int i = 1; i <= 3; i++) {
+        printf("Cheer %d: You are awesome, Ramathan! \n", i);
+    }
+
+    printf("\nKeep coding, keep growing!\n");
+
+    return 0;
+}
